5451ffda305bdae7ce468c2a2d62d3f664643ea6
issam <issamyayou@gmail.com>
2021-09-07 12:11:24 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+#git push -f git@github.com:aissamyekhlef/aissamyekhlef.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:aissamyekhlef/di-statistics-vuejs.git main:gh-pages
 
 cd -
