64a4841d0a595aa5da957b057bee7f40c25fe1d2
issam <issamyayou@gmail.com>
2021-10-19 23:04:29 +0100

    deploy

---
+++ dist/precache-manifest.23c1d70177d03f1e70d88fe2c4bb7a70.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e672e22d6cdf1728cc6",
+    "url": "/di-statistics-vuejs/css/app.27f4ec0e.css"
+  },
+  {
+    "revision": "09b130ffc5bb2a37e570",
+    "url": "/di-statistics-vuejs/css/chunk-vendors.1671cb0c.css"
+  },
+  {
+    "revision": "42a36e3fe75a5f30d1fab266e24e1487",
+    "url": "/di-statistics-vuejs/favicon-32x32.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/di-statistics-vuejs/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/di-statistics-vuejs/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/di-statistics-vuejs/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/di-statistics-vuejs/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/di-statistics-vuejs/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "09aea0f59807f6f4f66af7f5719cba9e",
+    "url": "/di-statistics-vuejs/img/google.09aea0f5.svg"
+  },
+  {
+    "revision": "3d432ca2badb7e0130b379f9162b99b1",
+    "url": "/di-statistics-vuejs/img/logo.3d432ca2.svg"
+  },
+  {
+    "revision": "c5678cd136022b24ee732fc9aa3fa683",
+    "url": "/di-statistics-vuejs/index.html"
+  },
+  {
+    "revision": "9e672e22d6cdf1728cc6",
+    "url": "/di-statistics-vuejs/js/app.c5a915dc.js"
+  },
+  {
+    "revision": "09b130ffc5bb2a37e570",
+    "url": "/di-statistics-vuejs/js/chunk-vendors.47751dfd.js"
+  },
+  {
+    "revision": "af541a5216251284a964d007e5bb27ee",
+    "url": "/di-statistics-vuejs/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/di-statistics-vuejs/robots.txt"
+  }
+]);
